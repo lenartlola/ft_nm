c48c871d8efa700f83f2db5a73181da202c5b570
lenart <lenartlola@proton.me>
2023-04-21 12:26:51 +0200

    Add some error handlers

--- src/init_utils.c
+++ src/init_utils.c
@@ -6,7 +6,6 @@
 
 int map_data(t_data *data, char *prog)
 {
-	(void )data;
 	struct stat	stat_buf;
 	int     fd;
 	char    *mptr;
@@ -16,18 +15,23 @@ int map_data(t_data *data, char *prog)
 		ft_printf(1, "ft_nm: '%s': No such file\n", prog);
 		return 1;
 	}
-	if (fstat(fd, &stat_buf) == -1)
+	if (fstat(fd, &stat_buf) == -1) {
+		close(fd);
 		return 1;
+	}
 	mptr = mmap(NULL, stat_buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	if (mptr == MAP_FAILED) {
 		ft_printf(0, "Error: failed to map file %s.\n", prog);
+		close(fd);
 		return 1;
 	}
 	close(fd);
-	int hdr_magic = *(int *)(mptr);
-	if (hdr_magic != ELF_LITTLE_MAGIC && hdr_magic != ELF_BIG_MAGIC)
+	uint32_t hdr_magic = *(uint32_t *)(mptr);
+	if (hdr_magic != ELF_LITTLE_MAGIC && hdr_magic != ELF_BIG_MAGIC) {
+		ft_printf(0, "Error: not an elf file %s\n", prog);
+		munmap(mptr, stat_buf.st_size);
 		return 1;
+	}
     add_p_lst_back(&data->p_lst, add_prog(prog, mptr, stat_buf));
 	return 0;
 }
-
